c75f1a76bc2575cb45f67860684431adc374019b
zifan <j22melody@gmail.com>
2018-06-20 18:17:33 +0800

    different open-id

--- pages/landing/landing.js
+++ pages/landing/landing.js
@@ -6,6 +6,9 @@ Page({
   data: {
   },
   onLoad: function () {
+    this.setData({
+      openId: Math.random().toString().slice(2),
+    })
   },
   message: function (e) {
     wx.setStorageSync('coupon', e.detail.data[0].coupon)
